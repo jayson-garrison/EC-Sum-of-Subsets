5b77a91920530b31c92916d7d34687bcadbf566d
Jayson C. Garrison <jcg5493@utulsa.edu>
2022-02-12 23:28:44 -0600

    added structure, info, and basic GA

---
+++ project/Utils/GenericGA.py
@@ -0,0 +1,18 @@
+# basic interface for a generic genetic algorithm
+
+def GeneticAlgorithm():
+
+    def __init__(self):
+        pass
+
+    def select_parents(pool):
+        # need fitness and selection routines (roulette, etc.)
+        pass
+
+    def crossover(parent1, parent2):
+        pass
+
+    def mutation(rate):
+        pass
+
+    # where do we put fitness fx?
